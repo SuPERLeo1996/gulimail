5754b634e21eabab29e3c9687ed808f0c9d69fb2
gonglinjie <gonglinjie>
2020-06-03 10:44:58 +0800

    调整

--- mall-product/src/main/java/com/leo/mall/product/controller/SpuCommentController.java
+++ mall-product/src/main/java/com/leo/mall/product/controller/SpuCommentController.java
@@ -1,20 +1,14 @@
 package com.leo.mall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.leo.mall.product.entity.SpuCommentEntity;
-import com.leo.mall.product.service.SpuCommentService;
 import com.leo.common.utils.PageUtils;
 import com.leo.common.utils.R;
+import com.leo.mall.product.entity.SpuCommentEntity;
+import com.leo.mall.product.service.SpuCommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 
